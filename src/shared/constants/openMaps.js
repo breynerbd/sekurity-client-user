304423b2e0ab0fdcec5c2ca42b061601f98a040b
breynerbd <bbenitez-2021497@kinal.edu.gt>
2026-07-31 17:51:20 -0600

    Solución de abrir unicación dentro de la web

--- src/shared/constants/openMaps.js
+++ src/shared/constants/openMaps.js
@@ -3,13 +3,19 @@ import { Linking, Platform } from 'react-native';
 export const openMaps = (latitude, longitude) => {
     if (!latitude || !longitude) return;
 
+    if (Platform.OS === 'web') {
+        const webUrl = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+        window.open(webUrl, '_blank');
+        return;
+    }
+
     const url = Platform.select({
-        ios: `maps://app?daddr=${latitude},${longitude}`,
-        android: `google.navigation:q=${latitude},${longitude}`
+        ios: `maps://app?q=${latitude},${longitude}`,
+        android: `geo:${latitude},${longitude}?q=${latitude},${longitude}`
     });
 
     Linking.openURL(url).catch(() => {
-        const webUrl = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`;
+        const webUrl = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
         Linking.openURL(webUrl);
     });
 };
